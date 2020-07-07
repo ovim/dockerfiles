0a31d38424b9d1b6fcf85f0bab5db63bf75d9f97
汪玉龙 <2556811689@qq.com>
2020-07-07 20:09:52 +0800

    fix

--- wwwroot/default/redis.php
+++ wwwroot/default/redis.php
@@ -1,5 +1,5 @@
 <?php
 $redis = new Redis();
-$redis->connect('Redis的IP地址',6379);
+$redis->connect('172.19.0.21',6379);// 172.19.0.21 为redis的IP地址
 $redis->set('test','hello docker_redis');
 echo $redis->get('test');
